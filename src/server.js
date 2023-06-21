54abf7007853debe058d7a4209a35241a413280e
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-06-21 06:34:32 +0300

    project setup/added express

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-undef */
 import 'dotenv/config'
 
-console.log('Hello world. Howdy nodemon!')
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () =>
+  console.log('App is hot and listening on port 3000!'),
+);
